102b86ac07e989beace86d2272b2be231d03b196
DAKSHM77 <masiyavad07@gmail.com>
2025-12-31 15:08:02 +0530

    Initial commit

--- app/views/layouts/admin_footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-</div>
-</div>
-<script src="<?php echo asset('js/jquery.js'); ?>"></script>
-<script src="<?php echo asset('js/bootstrap.min.js'); ?>"></script>
-</body>
-
-</html>
---
+++ student/organizer/organizer_styles.php
@@ -0,0 +1,19 @@
+<style>
+    /* Adjust header height and visibility for Organizer Dashboard */
+    .navbar {
+        min-height: 80px;
+        background-color: #fff;
+        border-bottom: 1px solid #ddd;
+    }
+    .navbar-nav > li > a {
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+    .navbar-brand {
+        height: 80px;
+        padding-top: 30px;
+    }
+    .navbar-toggle {
+        margin-top: 23px;
+    }
+</style>
